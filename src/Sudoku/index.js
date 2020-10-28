eb33cd71f9a0f03c59574b84154221e194aa17a6
jiaoyueyue <jiaoyueyue@baidu.com>
2020-10-28 09:59:10 +0800

    九宫格代码优化

--- src/Sudoku/index.js
+++ src/Sudoku/index.js
@@ -1,24 +1,27 @@
-var colorList = [];
-var gridList = [];
-var intervalBox;
+/**
+ * @file 变色九宫格
+ */
+let colorList = [];
+let gridList = [];
+let intervalBox;
 
 window.onload = function () {
     window.document.getElementById('start').onclick = function () {
         changeColor();
     };
     window.document.getElementById('stop').onclick = function () {
-        clearInterval(intervalBox)
+        clearInterval(intervalBox);
     };
 };
 
 
-function randomHexColor() { //随机生成十六进制颜色
-    if (colorList.length = 3) {
+function randomHexColor() { // 随机生成十六进制颜色
+    if (colorList.length ===  3) {
         colorList.length = 0;
     }
-    for (var i = 0; i < 3; i++) {
-        var hex = Math.floor(Math.random() * 16777216).toString(16); // 生成ffffff以内16进制数
-        //while循环判断hex位数，少于6位前面加0凑够6位
+    for (let i = 0; i < 3; i++) {
+        let hex = Math.floor(Math.random() * 16777216).toString(16); // 生成ffffff以内16进制数
+        // while循环判断hex位数，少于6位前面加0凑够6位
         while (hex.length < 6) {
             hex = '0' + hex;
         }
@@ -27,24 +30,24 @@ function randomHexColor() { //随机生成十六进制颜色
 }
 
 function randomGrid() {
-    if (gridList.length = 3) {
+    if (gridList.length === 3) {
         gridList.forEach(label => {
             document.getElementById(label).style.backgroundColor = 'coral';
         });
         gridList.length = 0;
     }
-    for (var j = 0; j < 3; j++) {
+    for (let j = 0; j < 3; j++) {
         gridList.push(Math.floor(Math.random() * 9 + 1));
     }
 }
 
 function changeColor() {
-    clearInterval(intervalBox)
+    clearInterval(intervalBox);
     intervalBox = setInterval(() => {
         randomHexColor();
         randomGrid();
         gridList.forEach((grid, index) => {
             document.getElementById(grid).style.backgroundColor = colorList[index];
-    })
+        });
     }, 1000);
 }
